e53ec95b8f8f1e2126902c95c2e07baa84339db7
Abhi solanki <abhi85076@gmail.com>
2026-06-12 16:30:09 +0530

    feat: read and write in tasks.json

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -1,6 +1,7 @@
 import fs from "fs/promises"
 
 const USERS_FILE = "src/data/users.json"
+const TASKS_FILE = "src/data/tasks.json"
 
 // To read users from file
 export const readUsers = async () => {
@@ -11,4 +12,15 @@ export const readUsers = async () => {
 // to save users in file
 export const writeUsers = async (users) => {
     await fs.writeFile(USERS_FILE, JSON.stringify(users, null, 2))
+}
+
+// to read tasks from tasks.json
+export const readTasks = async () => {
+    const data = await fs.readFile(TASKS_FILE, "utf-8")
+    return JSON.parse(data)
+}
+
+//to save tasks in file
+export const writeTasks = async (tasks) => {
+    await fs.writeFile(TASKS_FILE, JSON.stringify(tasks, null, 2))
 }
