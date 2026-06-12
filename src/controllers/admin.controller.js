0b9894bb526cdfdc7964b9308db940fa6359cde6
Abhi solanki <abhi85076@gmail.com>
2026-06-12 17:32:20 +0530

    feat: Get all tasks for admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-import { readUsers } from "../utils/fileHandler.js";
+import { readTasks, readUsers } from "../utils/fileHandler.js";
 
 async function getAllUsersForAdmin(req, res) {
   try {
@@ -26,4 +26,38 @@ async function getAllUsersForAdmin(req, res) {
   }
 }
 
-export { getAllUsersForAdmin };
+async function getAllTasksForAdmin(req, res) {
+     try {
+        const tasks = await readTasks()
+
+        const users = await readUsers()
+
+        const allTasks = tasks.map(task => {
+            const user = users.find(u => u.id === task.userId)
+
+            return {
+                id: task.id,
+                title: task.title,
+                description: task.description,
+                status: task.status,
+                createdAt: task.createdAt,
+                userId: task.userId,
+                username: user ? user.username : "Unknown"
+            }
+        })
+
+        res.status(200).json({
+            message: "All tasks fetched",
+            tasks: allTasks
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
+}
+
+export { getAllUsersForAdmin, getAllTasksForAdmin };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import requireAuth from "../middlewares/requireAuth.js";
 import requireAdmin from "../middlewares/requiredAdmin.js";
-import { getAllUsersForAdmin } from "../controllers/admin.controller.js";
+import { getAllTasksForAdmin, getAllUsersForAdmin } from "../controllers/admin.controller.js";
 
 const adminRoute = Router()
 
+// GET /api/admin/users
 adminRoute.get('/users', requireAuth, requireAdmin, getAllUsersForAdmin)
+
+// GET /api/admin/tasks
+adminRoute.get('/tasks', requireAuth, requireAdmin, getAllTasksForAdmin)
 
 export default adminRoute
